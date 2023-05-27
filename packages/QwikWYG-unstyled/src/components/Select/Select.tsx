5d9d1b91d5a6134ee80072e12d791ee4e72fee44
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-05-27 21:47:51 +0530

    Minimum Required Component's file structure created (#3)
    * Create Layout.tsx
    
    * Create Row.tsx
    
    * Create Column.tsx
    
    * Create Box.tsx
    
    * Create Grid.tsx
    
    * Create Button.tsx
    
    * Create Input.tsx
    
    * Create Select.tsx
    
    * Create Slider.tsx
    
    * Create Switch.tsx
    
    * Create Badge.tsx
    
    * Create Snackbar.tsx
    
    * Create Menu.tsx
    
    * Create Accordion.tsx
    
    * Create Alert.tsx
    
    * Create Avatar.tsx
    
    * Create Breadcrumb.tsx
    
    * Create Card.tsx
    
    * Create Checkbox.tsx
    
    * Create Datepicker.tsx
    
    * Create Dialog.tsx
    
    * Create Disclosure.tsx
    
    * Create File.tsx
    
    * Create Icon.tsx
    
    * Create Image.tsx
    
    * Create List.tsx
    
    * Create Popup.tsx
    
    * Create Radio.tsx
    
    * Create Skeleton.tsx
    
    * Create Table.tsx
    
    * Create Tab.tsx
    
    * Create Text.tsx
    
    * Create Toast.tsx
    
    * Create Tooltip.tsx

---
+++ packages/QwikWYG-unstyled/src/components/Select/Select.tsx
@@ -0,0 +1,5 @@
+import { component$ } from '@builder.io/qwik';
+
+export const Select = component$(() => {
+  return(<></>);
+});
---
+++ packages/QwikWYG-unstyled/src/components/Snackbar/Snackbar.tsx
@@ -0,0 +1,5 @@
+import { component$ } from '@builder.io/qwik';
+
+export const Snackbar = component$(() => {
+  return(<></>);
+});
